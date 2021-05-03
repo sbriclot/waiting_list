d6381abff09e20ecfd3e8c0bcf1092d5178dab81
Stephane Briclot <stephane.briclot@gmail.com>
2021-05-03 18:32:20 +0200

    Fix bio length counter error on render

--- app/javascript/components/requests/bio_remaining_chars.js
+++ app/javascript/components/requests/bio_remaining_chars.js
@@ -1,13 +1,14 @@
 const bio_remaining_chars = () => {
   const bio_textarea = document.getElementById("request_bio");
   if (bio_textarea) {
-    function refreshCounter(event) {
+    function refreshCounter() {
       const dspCounter = document.getElementById("bio-max-length");
       const max_bio_length = dspCounter.dataset.bioMaxLength;
       dspCounter.innerText = `Remaining characters: ${max_bio_length - bio_textarea.textLength} / ${max_bio_length}`;
     }
 
     bio_textarea.addEventListener("keyup", refreshCounter);
+    window.addEventListener("load", refreshCounter);
   }
 }
 
